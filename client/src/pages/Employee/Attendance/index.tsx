ec7b34201baf7a82213a10ecea4e12a451335e3b
slear08 <88441444+slear08@users.noreply.github.com>
2024-03-10 01:42:43 +0800

    refactor(attendance): added pagination on table

--- client/src/pages/Employee/Attendance/index.tsx
+++ client/src/pages/Employee/Attendance/index.tsx
@@ -1,37 +1,23 @@
-import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow
-} from '@/components/ui/table';
 import { GetUserAttendance } from '@/api/services/employee/Attendance';
 import { useEffect, useState } from 'react';
 import { useUserStore } from '@/store/useUserStore';
-
+import { useQuery } from '@tanstack/react-query';
+import { DataTable } from '@/components/DataTable';
+import { columns } from './columns';
 const Attendance = () => {
     const userId = useUserStore().userId;
 
-    const [attendanceData, setAttendanceData] = useState([]);
+    const { data } = useQuery({
+        queryFn: () => GetUserAttendance(userId),
+        queryKey: ['attendance']
+    });
+
     const [haveError, setHaveError] = useState(false);
 
-    useEffect(() => {
-        const fetchAttendance = async () => {
-            try {
-                const attendance = await GetUserAttendance(userId);
-                setAttendanceData(attendance);
-            } catch (error) {
-                console.error('Error fetching attendance:', error);
-            }
-        };
-        fetchAttendance();
-    }, [userId]);
-
+    console.log(data);
     useEffect(() => {
         // Check if any item in attendanceData has null values
-        const hasNullValues = attendanceData.some(
+        const hasNullValues = data?.some(
             (item: any) =>
                 item &&
                 item.time &&
@@ -44,8 +30,11 @@ const Attendance = () => {
 
         // Set haveError state based on the presence of null values
         setHaveError(hasNullValues);
-    }, [attendanceData]);
+    }, [data]);
 
+    if (!data) {
+        return <div>Loading</div>;
+    }
     return (
         <div className="w-full p-10">
             <div className="mb-10">
@@ -59,74 +48,7 @@ const Attendance = () => {
                     </p>
                 )}
             </div>
-            <div>
-                <Table>
-                    <TableCaption className="text-primary">List of attendance</TableCaption>
-                    <TableHeader className="bg-primary-foreground">
-                        <TableRow>
-                            <TableHead className="w-[150px]">Date</TableHead>
-                            <TableHead className="w-[150px]">Time In</TableHead>
-                            <TableHead className="w-[150px]">Break In</TableHead>
-                            <TableHead className="w-[150px]">Break Out</TableHead>
-                            <TableHead className="w-[150px]">Time Out</TableHead>
-                            <TableHead className="w-[150px]">Overtime In</TableHead>
-                            <TableHead className="w-[150px]">Overtime Out</TableHead>
-                            <TableHead className="w-[150px]">Remarks</TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody className="bg-accent-foreground ">
-                        {attendanceData?.map((item: any, index: any) => (
-                            <TableRow key={index}>
-                                <TableCell className="font-medium">
-                                    {new Date(item.date).toLocaleDateString('en-US', {
-                                        month: 'long',
-                                        day: 'numeric',
-                                        year: 'numeric'
-                                    })}
-                                </TableCell>
-                                <TableCell
-                                    className={`${item.time.timeIn == null ? 'bg-red-200 text-red-700' : ''}`}>
-                                    {item.time.timeIn
-                                        ? item.time.timeIn.substring(11, 19)
-                                        : 'ERROR'}
-                                </TableCell>
-                                <TableCell
-                                    className={`${item.time.breakIn == null ? 'bg-red-200 text-red-700' : ''}`}>
-                                    {item.time.breakIn
-                                        ? item.time.breakIn.substring(11, 19)
-                                        : 'ERROR'}
-                                </TableCell>
-                                <TableCell
-                                    className={`${item.time.breakOut == null ? 'bg-red-200 text-red-700' : ''}`}>
-                                    {item.time.breakOut
-                                        ? item.time.breakOut.substring(11, 19)
-                                        : 'ERROR'}
-                                </TableCell>
-                                <TableCell
-                                    className={`${item.time.timeOut == null ? 'bg-red-200 text-red-700' : ''}`}>
-                                    {item.time.timeOut
-                                        ? item.time.timeOut.substring(11, 19)
-                                        : 'ERROR'}
-                                </TableCell>
-                                <TableCell>
-                                    {item.time.overTimeIn
-                                        ? item.time.overTimeIn.substring(11, 19)
-                                        : 'No Overtime'}
-                                </TableCell>
-                                <TableCell>
-                                    {item.time.overTimeIn
-                                        ? item.time.overTimeOut.substring(11, 19)
-                                        : 'No Overtime'}
-                                </TableCell>
-                                <TableCell
-                                    className={`${item.remarks == 'ERROR' ? 'bg-red-200 text-red-700' : ''}`}>
-                                    {item.remarks}
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </div>
+            <DataTable data={data} columns={columns} filter="_id" />
         </div>
     );
 };
