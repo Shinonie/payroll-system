8b97817cff1d6bcf61b8ef4724bd0479287dac38
slear08 <88441444+slear08@users.noreply.github.com>
2024-03-10 09:39:56 +0800

    feature(email): Added email for employee after registration

--- api/routes/AuthRoutes.js
+++ api/routes/AuthRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
 import { register, login, logout } from "../controllers/AuthController.js";
+import { generateEmailMiddleware } from "../middlewares/emailMiddleware.js";
 
-router.post("/register", register);
+router.post("/register", generateEmailMiddleware, register);
 router.post("/login", login);
 router.post("/logout", logout);
 
